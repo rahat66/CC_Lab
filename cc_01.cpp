71725616465b273983087e46760e7c5507e0dbe4
Rahat <Rahat>
2018-02-02 23:39:37 +0600

    add file i/o

--- cc_01.cpp
+++ cc_01.cpp
@@ -1,13 +1,30 @@
-/// problem-01: Identify comment.
+/// problem: Identify comment from a given file input.
 #include<cstdio>
 #include<cstring>
 int main()
 {
-    int len;
+    FILE *f1;
+    f1 =  fopen("newFile", "w");
+    char c;
+    int i =0,
+        len;
     bool flag = false;
-    char str[30];
-    gets(str);
-    len = strlen(str);
+    char str[1000];
+
+    while((c = getchar()) != EOF)
+        putc(c, f1);
+    fclose(f1);
+
+    f1 = fopen("newFile", "r");
+
+    while((c = getc(f1)) !=EOF){
+       //printf("%c",c);
+       str[i] = c;
+        i++;
+    }
+
+    fclose(f1);
+    len = i-1;
 
 /// check line commnet
     if(str[0] == '/')
@@ -15,6 +32,8 @@ int main()
         if(str[1] == '/')
             flag = true;
     }
+
+//    printf("%c %c", str[len-1],str[len-2]);
 
 /* check block comment */
     if(str[0] == '/' && str[1] == '*')
